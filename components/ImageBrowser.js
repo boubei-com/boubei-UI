2eee506d2e2d73f1a40970e3dac31cf5b606e579
afishhhhh <ygqdev0126@gmail.com>
2018-12-11 16:04:23 +0800

    Modify component

--- components/ImageBrowser.js
+++ components/ImageBrowser.js
@@ -1,1 +1,256 @@
-"use strict";var _slicedToArray=function(e,t){if(Array.isArray(e))return e;if(Symbol.iterator in Object(e))return function(e,t){var i=[],r=!0,a=!1,s=void 0;try{for(var n,o=e[Symbol.iterator]();!(r=(n=o.next()).done)&&(i.push(n.value),!t||i.length!==t);r=!0);}catch(e){a=!0,s=e}finally{try{!r&&o.return&&o.return()}finally{if(a)throw s}}return i}(e,t);throw new TypeError("Invalid attempt to destructure non-iterable instance")};!function(e){var t=function(){this.images=[],this.current=0,this.show=!1,this._dom,this.dom$imageName,this.dom$page,this.init()};function s(e,t){var i=window.getComputedStyle(e,null).getPropertyValue("transform"),r=i.split("(")[1].split(")")[0].split(","),a=_slicedToArray(r,2),s=a[0],n=a[1],o=Math.round(Math.atan2(n,s)*(180/Math.PI));e.style="-webkit-transform:rotate("+(o+t)+"deg);transform:rotate("+(o+t)+"deg)"}function n(e){var t=e.dom,i=e.offset;t.scrollLeft+=i}t.prototype={constructor:t,mount:function(e){return e.appendChild(this._dom),this},showOnClick:function(e){for(var t=e.getElementsByTagName("a"),i=[],r=0,a=t[r];void 0!==a;a=t[++r]){var s=a.getAttribute("href"),n=a.innerText;a.setAttribute("href","javascript:void(0);"),a.dataset.url=s,a.dataset.index=r,i.push({url:s,text:n})}this.insertImages(i),this.images=i,e.addEventListener("click",function(e){var i=this,t=e||window.event,r=t.target||t.srcElement;if("a"===r.nodeName.toLocaleLowerCase()){this.toggleClose();var a=+r.dataset.index;[(this.current=a)-1,a,a+1].forEach(function(e){var t=i.images[e];t&&t.dom.setAttribute("src",t.url)}),this._dom.getElementsByClassName("image-browser__image-swiper")[0].scrollLeft=600*a,this.dom$imageName.innerText=this.images[a].text,this.dom$page.innerText=this.current+1+"/"+this.images.length}}.bind(this),!1)},toggleClose:function(){if(this.show){this.show=!1;var e=this._dom.getElementsByClassName("image-browser__image-swiper")[0];e.scrollLeft=0,this._dom.classList.remove("image-browser__show"),this.current=0}else this.show=!0,this._dom.classList.add("image-browser__show")},lazyLoadImage:function(e){var t=this.images[e],i=t.url,r=t.dom;null===r.getAttribute("src")&&r.setAttribute("src",i)},init:function(){var e=this.create("div","image-browser"),t=this.createToolbar(),i=this.createImageNav(),r=this.createFooter();e.appendChild(t),e.appendChild(i),e.appendChild(r),this._dom=e,this.mount(document.body),this.showOnClick(document.getElementsByClassName("ext")[0])},create:function(e,t){var i=document.createElement(e);return i.className=t,i},createToolbar:function(){var e=this.create("div","image-browser__row image-browser__toolbar"),t=this.create("button","image-browser__btn image-browser__rotate-btn");t.appendChild(document.createTextNode("向左旋转"));var i=this.create("button","image-browser__btn image-browser__rotate-btn");i.appendChild(document.createTextNode("向右旋转"));var r=this.create("button","image-browser__btn image-browser__close-btn"),a=this.create("i","image-browser__btn-icon ion-ios-close-circle-outline");return r.appendChild(a),e.appendChild(t),e.appendChild(i),e.appendChild(r),t.onclick=function(){s(this.images[this.current].dom,-90)}.bind(this),i.onclick=function(){s(this.images[this.current].dom,90)}.bind(this),r.onclick=this.toggleClose.bind(this),e},createFooter:function(){var e=this.create("div","image-browser__row image-browser__footer"),t=this.create("div","image-browser__image-name");t.innerText="附件名称",this.dom$imageName=t;var i=this.create("div","image-browser__page");return i.innerText="0/0",this.dom$page=i,e.appendChild(t),e.appendChild(i),e},createImageNav:function(){var r=this,e=this.create("div","image-browser__row image-browser__image-nav"),t=this.create("div","image-browser__image-swiper"),i=function(e){var t=r.create("button","image-browser__btn image-browser__"+e+"-btn"),i=r.create("i","image-browser__btn-icon ion-ios-arrow-"+e);return t.appendChild(i),t},a=i("back"),s=i("forward");return e.appendChild(a),e.appendChild(t),e.appendChild(s),a.onclick=function(){if(0!=this.current){n({dom:t,offset:-600});var e=this.images.length;this.current=(this.current+e-1)%e,this.lazyLoadImage((this.current+e-1)%e),this.dom$imageName.innerText=this.images[this.current].text,this.dom$page.innerText=this.current+1+"/"+this.images.length}else alert("没有图片了")}.bind(this),s.onclick=function(){var e=this.images.length;this.current!=e-1?(n({dom:t,offset:600}),this.current=(this.current+1)%e,this.lazyLoadImage((this.current+1)%e),this.dom$imageName.innerText=this.images[this.current].text,this.dom$page.innerText=this.current+1+"/"+this.images.length):alert("没有图片了")}.bind(this),e},createImage:function(e){var t=e.src,i=e.index,r=this.create("img","image-browser__image");return r.setAttribute("alt",t),r.style="-webkit-transform:rotate(0);transform:rotate(0)",r.dataset.index=i,r},insertImages:function(e){var a=this,s=this._dom.getElementsByClassName("image-browser__image-swiper")[0];e.forEach(function(e,t){var i=a.createImage({src:e.url,index:t}),r=a.create("div","image-browser__image-wrapper");r.style.left=600*t+"px",r.appendChild(i),s.appendChild(r),e.dom=i})}},e.init=function(){return new t}}(window.ImageBrowser={}),ImageBrowser.init();
+;(function (window, factory) {
+  
+  factory((window.ImageBrowser = {}))
+  
+  ImageBrowser.init()
+
+}(window, (function (exports) {
+
+const ImageBrowser = function () {
+
+  this.images = []
+
+  this.current = 0
+
+  this.show = false
+
+  this._dom
+
+  this.dom$imageName
+
+  this.dom$page
+
+  this.init()
+}
+
+ImageBrowser.prototype = {
+
+  constructor: ImageBrowser,
+
+  mount (dom) {
+    dom.appendChild(this._dom)
+    return this
+  },
+  
+  showOnClick (dom) {
+    const tagAList = dom.getElementsByTagName('a')
+    
+    const images = []
+    for (let i = 0, link = tagAList[i]; link !== void 0; link = tagAList[++i]) {
+      const url = link.getAttribute('href')
+      const text = link.innerText
+      link.setAttribute('href', 'javascript:void(0);')
+      link.dataset.url = url
+      link.dataset.index = i
+      images.push({ url, text })
+    }
+    this.insertImages(images)
+    this.images = images
+    
+    function clickHandler (e) {
+      const event = e || window.event
+      const target = event.target || event.srcElement
+      if (target.nodeName.toLocaleLowerCase() !== 'a') {
+        return
+      }
+      this.toggleClose()
+      
+      const current = +target.dataset.index
+      this.current = current
+      
+      const loadIndex = [current - 1, current, current + 1]
+      loadIndex.forEach(i => {
+        const image = this.images[i]
+        image && image.dom.setAttribute('src', image.url)
+      })
+
+      const imageSwiper = this._dom.getElementsByClassName('image-browser__image-swiper')[0]
+      imageSwiper.scrollLeft = current * 600
+
+      this.dom$imageName.innerText = this.images[current].text
+      this.dom$page.innerText = `${this.current + 1}/${this.images.length}`
+    }
+
+    dom.addEventListener('click', clickHandler.bind(this), false)
+  },
+
+  toggleClose () {
+    if (this.show) {
+      this.show = false
+      const imageSwiper = this._dom.getElementsByClassName('image-browser__image-swiper')[0]
+      imageSwiper.scrollLeft = 0
+      this._dom.classList.remove('image-browser__show')
+      this.current = 0
+    }
+    else {
+      this.show = true
+      this._dom.classList.add('image-browser__show')
+    }
+  },
+
+  lazyLoadImage (index) {
+    const { url, dom } = this.images[index]
+    if (dom.getAttribute('src') !== null) {
+      return
+    }
+    dom.setAttribute('src', url)
+  },
+
+  init () {
+    const browserWrapper = this.create('div', 'image-browser')
+    const toolbar = this.createToolbar()
+    const imageNav = this.createImageNav()
+    const footer = this.createFooter()
+    browserWrapper.appendChild(toolbar)
+    browserWrapper.appendChild(imageNav)
+    browserWrapper.appendChild(footer)
+
+    this._dom = browserWrapper
+
+    this.mount(document.body)
+    this.showOnClick(document.getElementsByClassName('ext')[0])
+  },
+
+  create (tag, className) {
+    const dom = document.createElement(tag)
+    dom.className = className
+    return dom
+  },
+  
+  createToolbar () {
+    const toolbar = this.create('div', 'image-browser__row image-browser__toolbar')
+
+    const rotateLeftBtn = this.create('button', 'image-browser__btn image-browser__rotate-btn')
+    rotateLeftBtn.appendChild(document.createTextNode('向左旋转'))
+
+    const rotateRightBtn = this.create('button', 'image-browser__btn image-browser__rotate-btn')
+    rotateRightBtn.appendChild(document.createTextNode('向右旋转'))
+
+    const closeBtn = this.create('button', 'image-browser__btn image-browser__close-btn')
+    const closeBtnIcon = this.create('i', 'image-browser__btn-icon ion-ios-close-circle-outline')
+    closeBtn.appendChild(closeBtnIcon)
+
+    toolbar.appendChild(rotateLeftBtn)
+    toolbar.appendChild(rotateRightBtn)
+    toolbar.appendChild(closeBtn)
+
+    rotateLeftBtn.onclick = function () { rotate(this.images[this.current].dom, -90) }.bind(this)
+
+    rotateRightBtn.onclick = function () { rotate(this.images[this.current].dom, 90) }.bind(this)
+
+    closeBtn.onclick = this.toggleClose.bind(this)
+
+    return toolbar
+  },
+
+  createFooter () {
+    const footer = this.create('div', 'image-browser__row image-browser__footer')
+
+    const imageName = this.create('div', 'image-browser__image-name')
+    imageName.innerText = '附件名称'
+    this.dom$imageName = imageName
+
+    const page = this.create('div', 'image-browser__page')
+    page.innerText = '0/0'
+    this.dom$page = page
+
+    footer.appendChild(imageName)
+    footer.appendChild(page)
+
+    return footer
+  },
+
+  createImageNav () {
+    const imageNav = this.create('div', 'image-browser__row image-browser__image-nav')
+
+    const imageSwiper = this.create('div', 'image-browser__image-swiper')
+
+    const createNavigatorBtn = arrow => {
+      const btn = this.create('button', `image-browser__btn image-browser__${arrow}-btn`)
+      const btnIcon = this.create('i', `image-browser__btn-icon ion-ios-arrow-${arrow}`)
+      btn.appendChild(btnIcon)
+
+      return btn
+    }
+
+    const backBtn = createNavigatorBtn('back')
+    const forwardBtn = createNavigatorBtn('forward')
+
+    imageNav.appendChild(backBtn)
+    imageNav.appendChild(imageSwiper)
+    imageNav.appendChild(forwardBtn)
+
+    backBtn.onclick = function () {
+      if (this.current == 0) {
+        alert('没有图片了')
+        return
+      }
+      scroll({ dom: imageSwiper, offset: -600 })
+      const len = this.images.length
+      this.current = (this.current + len - 1) % len
+      this.lazyLoadImage((this.current + len - 1) % len)
+
+      this.dom$imageName.innerText = this.images[this.current].text
+      this.dom$page.innerText = `${this.current + 1}/${this.images.length}`
+    }.bind(this)
+    
+    forwardBtn.onclick = function () {
+      const len = this.images.length
+      if (this.current == len - 1) {
+        alert('没有图片了')
+        return
+      }
+      scroll({ dom: imageSwiper, offset: 600 })
+      this.current = (this.current + 1) % len
+      this.lazyLoadImage((this.current + 1) % len)
+
+      this.dom$imageName.innerText = this.images[this.current].text
+      this.dom$page.innerText = `${this.current + 1}/${this.images.length}`
+    }.bind(this)
+
+    return imageNav
+  },
+
+  createImage ({ src, index }) {
+    const image = this.create('img', 'image-browser__image')
+    image.setAttribute('alt', src)
+    image.style = '-webkit-transform:rotate(0);transform:rotate(0)'
+    image.dataset.index = index
+
+    return image
+  },
+
+  insertImages (images) {
+    const imageSwiper = this._dom.getElementsByClassName('image-browser__image-swiper')[0]
+    images.forEach((e, index) => {
+      const image = this.createImage({ src: e.url, index })
+      const imageWrapper = this.create('div', 'image-browser__image-wrapper')
+      imageWrapper.style.left = `${index * 600}px`
+  
+      imageWrapper.appendChild(image)
+      imageSwiper.appendChild(imageWrapper)
+  
+      e.dom = image
+    })
+  }
+}
+
+function rotate (dom, deg) {
+  const matrix = window.getComputedStyle(dom, null).getPropertyValue('transform')
+  const [a, b] = matrix.split('(')[1].split(')')[0].split(',')
+  const _deg = Math.round(Math.atan2(b, a) * (180 / Math.PI))
+  dom.style = `-webkit-transform:rotate(${_deg + deg}deg);transform:rotate(${_deg + deg}deg)`
+}
+
+function scroll ({ dom, offset }) {
+  dom.scrollLeft += offset
+}
+
+function init () {
+  const imageBrowser = new ImageBrowser()
+
+  return imageBrowser
+}
+
+exports.init = init
+})));
